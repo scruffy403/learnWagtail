fde8dfc37bbfaf03a04f26ef0ece0939322f221f
John Soto <scruffy403@gmail.com>
2022-04-04 22:06:16 +0100

    add wagtail and config files

---
+++ rocketman/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-3_s%ihx1=#a4ivuf&8sej97(g43%*qwf4vhs)k50o1^f&9=n+0'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
