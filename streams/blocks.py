a87fbb686a56f3f7d9c84b8a87de734936063edd
John Soto <scruffy403@gmail.com>
2022-04-19 11:39:05 +0100

    added logic for cards StreamField

--- streams/blocks.py
+++ streams/blocks.py
@@ -15,15 +15,59 @@ class TitleBlock(blocks.StructBlock):
 
 
 
+class LinkValue(blocks.StructValue):
+    """Additional logic for our links """
+
+    def url(self) -> str:
+        internal_page = self.get("internal_page")
+        external_link = self.get("external_link")
+        if internal_page:
+            return internal_page.url
+        elif external_link:
+            return external_link
+        return ""
+
+
+
+class Link(blocks.StructBlock):
+    link_text = blocks.CharBlock(
+        max_length=50,
+        help_text="More details"
+    )
+    internal_page = blocks.PageChooserBlock(
+        required=False
+    )
+    external_link = blocks.URLBlock(
+        required=False
+    )
+
+    class Meta:
+        value_class = LinkValue
+
+
+
+
+class Card(blocks.StructBlock):
+    title = blocks.CharBlock(
+        max_length=100,
+        help_text="Bold title text for this card. Max length of 100 characters.",
+    )
+    text = blocks.TextBlock(
+        max_length=255,
+        help_text="Optional text for this card. Max length 255 characters.",
+        required=False
+    )
+    image = ImageChooserBlock(
+        help_text="Image will be automagically cropped to 570x370 pixels.",
+    )
+    link = Link(help_text="Enter a link or select a page")
+
+
+
 class CardsBlock(blocks.StructBlock):
+
     cards = blocks.ListBlock(
-        blocks.StructBlock(
-            [
-                ("title", blocks.CharBlock(max_length=100, help_text="Bold title text for this card. Max lenth 100 characters.")),
-                ("text", blocks.TextBlock(max_length=255, help_text="Optional text for this card. Max length is 255 characters.", required=False)),
-                ("image", ImageChooserBlock(help_text="Image will be automatically cropped to 570px x 370px.")),
-            ]
-        )
+        Card()
     )
 
     class Meta:
